79492f6bc1fd185bdf7d2c54e574e560f02d4a8c
Kirthiraj24 <141327641+Kirthiraj24@users.noreply.github.com>
2026-01-14 15:10:02 +0530

    Use MYSQL_URL and fix DB connection

--- config/db.js
+++ config/db.js
@@ -1,19 +1,14 @@
 const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-});
+const pool = mysql.createPool(process.env.MYSQL_URL);
 
-pool.getConnection((err) => {
+pool.getConnection((err, connection) => {
   if (err) {
     console.error("Database connection failed:", err.message);
   } else {
-    console.log("Database connected successfully");
+    console.log("MySQL connected successfully");
+    connection.release();
   }
 });
 
-module.exports = pool;
+module.exports = pool.promise();
